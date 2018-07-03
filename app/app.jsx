1b2bd7e6300bced828cfa2418dda2c5e1004fce3
Nicholas Summervill <nsummervill@yahoo.com>
2018-07-02 21:36:02 -0500

    add TodoApp.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
+var TodoApp = require('TodoApp');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -10,4 +10,4 @@ $(document).foundation();
 //App css
 require('style!css!sass!ApplicationStyles')
 
-ReactDOM.render(<p>Boilerplate 3</p>, document.getElementById('app'));
+ReactDOM.render(<TodoApp/>, document.getElementById('app'));
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function(){
+    return(
+      <div>
+        Todo App.jsx
+      </div>
+    )
+
+  }
+});
+
+module.exports = TodoApp;
